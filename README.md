2321a7a2a07c45dec66e493e99cfe263085a2325
Alex Tiley <alex@tiley.io>
2018-07-14 22:06:03 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# wdio-cucumber-missing-steps-reporter
+Reports missing cucumber steps when running the wdio test runner
+
+## Usage ##
+1. To install, run `yarn add wdio-cucumber-missing-steps-reporter` or `npm install wdio-cucumber-missing-steps-reporter`
+2. Add 'missing-steps' to the list of reporters in `wdio.conf.js`.
+3. Run your tests
+
+## Bug reporting ##
+
+Feel free to raise a pull request, or throw me a ticket via the issues section.
---
+++ src/reporter.js
@@ -0,0 +1,128 @@
+/**
+ * @class {EventEmitter}
+ */
+const EventEmitter = require('events');
+const fs = require('fs');
+const path = require('path');
+const Gherkin = require('gherkin');
+
+class CucumberMissingStepsReporter extends EventEmitter {
+
+  constructor(baseReporter, config, options = {}) {
+    super();
+
+    if (
+      typeof options.cucumberMissingStepsReporter === 'undefined' ||
+      typeof options.cucumberMissingStepsReporter.baseDir === 'undefined'
+    ) {
+      console.log('Unable to generate missing step snippets: missing reporters.cucumberMissingStepsReporter.baseDir in wdio.conf.js config file.');
+      return;
+    }
+
+    this.baseReporter = baseReporter;
+    this.config = config;
+    this.cachedFeatures = {};
+    this.options = options;
+    this.snippets = [];
+
+    this.on('test:pending', this.build);
+    this.on('end', this.notify);
+  }
+
+  build(spec) {
+    // Get step (and its keyword + text) from .feature file
+    // We can not use spec.title as this contains substituted placeholders (scenario outline data)
+    const step = this.getStep(spec.file, this.getLineNumberFromUid(spec.uid));
+
+    if (step.keyword !== 'After' && step.keyword !== 'Before') {
+      if (spec.title.indexOf(' (undefined step)') > -1) {
+        this.snippets.push(
+          step.keyword.trim() + '(/^' + step.text + '$/, () => {\n\t// Implement me!\n});'
+        );
+      }
+    }
+  }
+
+  notify() {
+    this.snippets.forEach((snippet) => {
+      console.log('\n');
+      console.log('\x1b[33m%s\x1b[0m', snippet);
+      console.log('\n');
+    });
+  }
+
+  /**
+   * Parses a gherkin feature file and transforms to JSON
+   * @param filePath - the path to the gherkin .feature file
+   * @returns {number|*}
+   */
+  parseFeature(filePath) {
+    const featurePath = path.join(this.options.cucumberJsonReporter.baseDir, filePath);
+    const featureText = fs.readFileSync(featurePath).toString();
+    const parser = new Gherkin.Parser(new Gherkin.AstBuilder());
+    const scanner = new Gherkin.TokenScanner(featureText);
+    const matcher = new Gherkin.TokenMatcher();
+
+    return parser.parse(scanner, matcher);
+  }
+
+  /**
+   * Given a gherkin file path and line number, finds the step with the relevant
+   * .feature file and returns the step's meta data.
+   *
+   * @param filePath
+   * @param stepLineNumber
+   * @returns {{}}
+   */
+  getStep(filePath, stepLineNumber) {
+    let step = {};
+    const feature = this.getFeature(filePath);
+
+    feature.children.forEach((scenario) => {
+      scenario.steps.forEach((currStep) => {
+        if (currStep.location.line === stepLineNumber) {
+          step = currStep;
+        }
+      });
+    });
+
+    return step;
+  }
+
+  /**
+   * Given a gherkin file path, finds the feature within the .feature file
+   * and returns the feature's meta data
+   *
+   * @param filePath - the path to the gherkin .feature file
+   * @returns {*}
+   */
+  getFeature(filePath) {
+    if (typeof this.cachedFeatures[filePath] !== 'undefined') {
+      return this.cachedFeatures[filePath];
+    }
+    const document = this.parseFeature(filePath);
+
+    this.cachedFeatures[filePath] = document.feature;
+
+    return document.feature;
+  }
+
+  /**
+   * The UID returned from wdio contains the step's line number from the .feature file
+   * This helper will extract it from the UID string
+   * @param uid
+   * @returns {number}
+   */
+  getLineNumberFromUid(uid) {
+    let line = uid.match(/\d+$/);
+
+    if (line === null || !Array.isArray(line) || isNaN(Number(line[0]))) {
+      return -1;
+    }
+    return Number(line);
+  }
+}
+
+CucumberMissingStepsReporter.reporterName = 'missing-steps';
+
+module.exports = CucumberMissingStepsReporter;
